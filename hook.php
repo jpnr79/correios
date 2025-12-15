cfcd465af34e7874c9adfe54f8bc6ca9c576f507
Pedro Rocha <jpnr79@gmail.com>
2025-12-15 00:10:43 +0000

    Add GLPI_ROOT definition and implement plugin functions for correios

--- hook.php
+++ hook.php
@@ -1,4 +1,5 @@
 <?php
+if (!defined('GLPI_ROOT')) { define('GLPI_ROOT', realpath(__DIR__ . '/../..')); }
 function correios_hook_function($parm) {
     // Aqui você pode adicionar ações específicas se necessário
 }
